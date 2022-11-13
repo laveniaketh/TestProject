101255b4af69c3be044c782f251470d691c7db12
kethl <kethl@laveniaketh>
2022-11-13 20:03:30 +0800

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kethl
+ *
+ */
+module testproject {
+}
---
+++ src/testproject/testproject.java
@@ -0,0 +1,10 @@
+package testproject;
+
+public class testproject {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
