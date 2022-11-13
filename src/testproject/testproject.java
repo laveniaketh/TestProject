8ea95121952ad6a8106f81dae2f117259921a5e7
kethl <kethl@laveniaketh>
2022-11-13 20:24:03 +0800

    updated testproject.java

--- src/testproject/testproject.java
+++ src/testproject/testproject.java
@@ -3,7 +3,7 @@ package testproject;
 public class testproject {
 
 	public static void main(String[] args) {
-		System.out.println("Hakdog ka");
+		System.out.println("ako si keth lavene abunda");
 
 	}
 
